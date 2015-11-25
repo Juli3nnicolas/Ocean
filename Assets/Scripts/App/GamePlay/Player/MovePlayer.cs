5d3d6d6853781d41016873ca647a0e310869e51f
Julien Nicolas <juli3nnicolas@gmail.com>
2015-11-25 13:21:59 +0100

    Added namespaces to better structure the project. Added the StdInterfaces folder. Added a script to set the player in motion.

---
+++ Assets/Scripts/App/GamePlay/Player/MovePlayer.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using Core.StdInterfaces;
+
+namespace App
+{
+	namespace Gameplay
+	{
+		public class MovePlayer : MonoBehaviour, Playable 
+		{
+			/////////// P L A Y A B L E   I N T E R F A C E ///////////
+			
+			// Start off object execution
+			public void init()
+			{
+			
+			}
+			
+			// Resume object execution
+			public void play ()
+			{
+			
+			}
+			
+			// Stop object execution
+			public void stop ()
+			{
+			
+			}
+			
+			// End object execution
+			public void terminate()
+			{
+			
+			}
+			
+			/////////// M O N O   B E H A V I O R ///////////
+			
+			// Use this for initialization
+			void Start ()
+			{
+			
+			}
+			
+			// Update is called once per frame
+			void Update () 
+			{
+			
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Core/StdInterfaces/Playable.cs
@@ -0,0 +1,23 @@
+﻿// Enable an object to be played or stopped.
+// Useful when a behavior must be scheduled to happen in a complex way.
+
+namespace Core
+{
+	namespace StdInterfaces
+	{
+		public interface Playable {
+		
+			// Start off object execution
+			void init();
+		
+			// Resume object execution
+			void play ();
+			
+			// Stop object execution
+			void stop ();
+			
+			// End object execution
+			void terminate();
+		}
+	}
+}
