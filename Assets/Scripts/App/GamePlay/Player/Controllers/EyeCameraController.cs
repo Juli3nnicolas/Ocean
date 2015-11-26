2b2e0abe01fd8e887045f1ee21317db50401ddff
Julien Nicolas <juli3nnicolas@gmail.com>
2015-11-26 04:48:58 +0100

    Camera controller improvements

--- Assets/Scripts/App/GamePlay/Player/Controllers/EyeCameraController.cs
+++ Assets/Scripts/App/GamePlay/Player/Controllers/EyeCameraController.cs
@@ -21,25 +21,62 @@ namespace App
 				// Update is called once per frame
 				void Update () 
 				{
+					float dt = Time.deltaTime;
+					makeForwardRotations(dt);
+					makeSideRotations(dt);
+				}
+				
+				// Rotate from side to side
+				private void makeSideRotations(float deltaTime)
+				{
 					float mouse_h = Input.GetAxis("Mouse X");
+					
+					// Rotate to the left
+					if ( m_angle.y > -90.0f && mouse_h < 0 )
+					{
+						float new_angle = m_angle.y + mouse_h*m_angularSpeed.y*deltaTime;
+						p_Camera.transform.RotateAround(p_Camera.transform.position, m_forwardAxis, new_angle - m_angle.y);
+						m_angle.y = new_angle;
+					}
+					
+					// Rotate to the right
+					if ( m_angle.y < 90.0f && mouse_h > 0 )
+					{
+						float new_angle = m_angle.y + mouse_h*m_angularSpeed.y*deltaTime;
+						p_Camera.transform.RotateAround(p_Camera.transform.position, m_forwardAxis, new_angle - m_angle.y);
+						m_angle.y = new_angle;
+					}
+				}
+				
+				// Rotate backward and forward
+				private void makeForwardRotations(float deltaTime)
+				{
 					float mouse_v = Input.GetAxis("Mouse Y");
-					float t = Time.deltaTime;
 					
-					Vector3 cur_angles = p_Camera.transform.rotation.eulerAngles;
+					// Rotate forward
+					if ( m_angle.x < 45.0f && mouse_v > 0 )
+					{
+						float new_angle = m_angle.x + mouse_v*m_angularSpeed.x*deltaTime;
+						p_Camera.transform.RotateAround(p_Camera.transform.position, m_sideAxis, new_angle - m_angle.x);
+						m_angle.x = new_angle;
+					}
 					
-					p_Camera.transform.Rotate(mouse_v*m_angularSpeed.y*t, mouse_h*m_angularSpeed.x*t, 0.0f);
-					
-					// WIP
-					//if ( cur_angles.x >= 45.0f && cur_angles.x <= 185.0f ) 
-					//	p_Camera.transform.Rotate(mouse_v*m_angularSpeed.y*t, 0.0f/*mouse_h*m_angularSpeed.x*t*/, 0.0f);
+					// Rotate backward
+					if ( m_angle.x > -20.0f && mouse_v < 0 )
+					{
+						float new_angle = m_angle.x + mouse_v*m_angularSpeed.x*deltaTime;
+						p_Camera.transform.RotateAround(p_Camera.transform.position, m_sideAxis, new_angle - m_angle.x);
+						m_angle.x = new_angle;
+					}
 						
-					//if ( cur_angles.y >= 0.0f && cur_angles.y <= 180.0f )
-					//	p_Camera.transform.Rotate(/*mouse_v*m_angularSpeed.y*t*/ 0.0f, mouse_h*m_angularSpeed.x*t, 0.0f);
 				}
 				
 				////////////// Attributes //////////////
 				
 				private Vector2 m_angularSpeed = new Vector2(30.0f, 30.0f); // In degree per seconds
+				private Vector2 m_angle        = new Vector2(0.0f, 0.0f);   // 1st coordinate: side; 2nd: forward
+				private Vector3 m_sideAxis     = new Vector3(1.0f, 0.0f, 0.0f);
+				private Vector3 m_forwardAxis  = new Vector3(0.0f, 0.0f, 1.0f);
 			}
 		}
 	}
