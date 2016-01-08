c91a9a0189e7b7fc6ccfab224d145963a92ef12b
Julien Nicolas <juli3nnicolas@gmail.com>
2016-01-08 13:00:47 +0100

    Workaround for Occulus camera management

---
+++ Assets/DontMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DontMove : MonoBehaviour {
+
+    void Awake()
+    {
+        m_firstPos = transform;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = m_firstPos.position;
+        transform.rotation = m_firstPos.rotation;
+    }
+
+    private Transform m_firstPos;
+}
